9100ef178a87ada3105267a4412bbc709b993db1
Jun-Seong Lee <leejs209209@gmail.com>
2020-06-03 14:41:37 +0900

    .

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ server = ModularServer(SchoolModel,
                        [grid, s_chart],
                        "School Model",
                        {"N": 1000, "group_N": 10, "width": 50, "height": 50,
-                        "initial_num_infected": 3, "infection_duration": 30})
+                        "initial_num_infected": 1, "infection_duration": 30})
 server.port = 8080
 server.launch()
 
