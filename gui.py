09ddc038ae12cf38545eb35a942ef055852da011
Jun-Seong Lee <leejs209209@gmail.com>
2020-07-30 12:33:46 +0900

    Improving GUI

--- gui.py
+++ gui.py
@@ -49,7 +49,8 @@ timetable_extra = timetable_normal + [
     'recess_cont'
 ]
 
-asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())  # Windows 서버 실행오류 해결
 
 sg.theme('LightGrey1')
 parameter_list = [('N', 1050), ('N_per_group', 25), ('width', 140), ('height', 61), ('initial_num_infected', 1),
@@ -59,11 +60,14 @@ parameter_list = [('N', 1050), ('N_per_group', 25), ('width', 140), ('height', 6
                   ('meal_random', True), ('meal_distanced', False), ('dinner_percentage', 0.3),
                   ('timetable', True), ('split_opening', False)
                   ]
+
 layout = [[sg.Text('변인설정', text_color='blue')]]
-
 layout += [[sg.Text(a), sg.InputText(str(b), key=a)] for a, b in parameter_list]
-
 layout += [[sg.Button('시각화', bind_return_key=True), sg.Button('닫기')]]
+layout += [[sg.Text("데이터 추출하기")]]
+layout += [[sg.Text("가설 이름"), sg.InputText("가설 1번", key="hypothesis_name")]]
+layout += [[sg.InputText('3', key="run_repeat_no"), sg.Text("회 반복하기")]]
+# layout += [[sg.Output(size=(50, 10))]]
 
 window = sg.Window('Python을 이용한 감염병 시뮬레이션', layout)
 
@@ -74,6 +78,8 @@ while True:
         break
 
     if event == '시각화':
+        values.pop("hypothesis_name")
+        values.pop("run_repeat_no")
         for x in values.keys():
             if values[x] == 'True' or values[x] == 'False':
                 values[x] = bool(values[x])
@@ -83,26 +89,9 @@ while True:
                 except ValueError:
                     values[x] = float(values[x])
         values['timetable'] = timetable_extra if values['timetable'] else timetable_normal
-        # if value 에서 timetable_normal == True: timetable = timetable_normal else: timetable =
-        # timetable_extra
-        print(values)
-        print(values == {"N": 1050, "N_per_group": 25, "width": 140, "height": 61,
-                             "initial_num_infected": 1, "infection_duration": 7,  "exposed_duration": 5,
-                             "infection_prob_per_contact": 0.2, "exposed_infection_prob_per_contact": 0.1,
-                             "restaurant_multiplier": 3, "visit_prob_per_person": 0.1,
-                             "meal_random": False, "meal_distanced": False, "dinner_percentage": 0.3,
-                             "timetable": timetable_normal, "split_opening": False})
-        server = ModularServer(SchoolModel,
-                               [grid, s_chart],
-                               "School Model",
-                               values)
 
-        # {"N": 1050, "N_per_group": 25, "width": 140, "height": 61,
-        #                      "initial_num_infected": 1, "infection_duration": 7,  "exposed_duration": 5,
-        #                      "infection_prob_per_contact": 0.2, "exposed_infection_prob_per_contact": 0.1,
-        #                      "restaurant_multiplier": 3, "visit_prob_per_person": 0.1,
-        #                      "meal_random": False, "meal_distanced": False, "dinner_percentage": 0.3,
-        #                      "timetable": timetable_normal, "split_opening": False}
+        print("인자:", values)
+        server = ModularServer(SchoolModel, [grid, s_chart], "감염병 모델 시각화", values)
 
         server.port = 8081
 
